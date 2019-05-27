b1f6b102a60925f6dcac2794ce811a9ae981d66c
Saarsalmi <perttueerojuhani.saarsalmi@cgi.com>
2019-05-27 14:43:37 +0300

    osa1 laskutustiedot finished

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Header = (props) => {
+
+    return(<div>
+        <p>
+       {props.course}
+    </p>
+    </div>)
+}    
+
+const Part = (props) => {
+    
+
+    return(
+    <>
+        {props.part} {props.exercises}
+    </>
+    )
+}
+
+const Content = (props) => {
+
+    return(
+    <div>    
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+    )
+
+}
+
+const Total = (props) => {
+
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
+const App = () => {
+    // const-määrittelyt
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+  
+    return (
+      <div>
+        <Header course={course} />
+        <Content part1={part1} exercises1={exercises1}/>
+        <Content part2={part2} exercises2={exercises2}/>
+        <Content part3={part3} exercises3={exercises3}/>
+        <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      </div>
+    )
+  }
+
+ReactDOM.render(<App />, document.getElementById('root'))
